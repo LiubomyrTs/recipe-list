9d6b45bb8d8c48474f9d8662f9b45ace8f2382ca
Liubomyr Tsimbolynets <liubomyrt@gmail.com>
2020-02-04 12:38:21 +0200

    Updates

---
+++ precache-manifest.d0addd2795cd211f5a6f4dce5dbd3b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef317fc4325ad52b193f1f2f68ba2753",
+    "url": "/recipe-list/index.html"
+  },
+  {
+    "revision": "ef990ba56184b10640ea",
+    "url": "/recipe-list/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7ae4a936a7b318b3da74",
+    "url": "/recipe-list/static/css/main.9d1f3aa1.chunk.css"
+  },
+  {
+    "revision": "ef990ba56184b10640ea",
+    "url": "/recipe-list/static/js/2.895f6039.chunk.js"
+  },
+  {
+    "revision": "7ae4a936a7b318b3da74",
+    "url": "/recipe-list/static/js/main.e6204baf.chunk.js"
+  },
+  {
+    "revision": "899aa4bf66c93f840923",
+    "url": "/recipe-list/static/js/runtime~main.b8fc7864.js"
+  }
+]);
